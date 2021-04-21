c1bbdf244809e0ee3703c96ece53222c2ca90e29
Paolo Bernacca <bernaccapaolo@gmail.com>
2021-04-21 23:43:18 +0200

    Refactoring. Moved to PyCharm

--- src/NN/ActorCriticNetwork.py
+++ src/NN/ActorCriticNetwork.py
@@ -1,7 +1,10 @@
-import tensorflow as tf      # Deep Learning library
+from collections import deque
+import tensorflow as tf  # Deep Learning library
 import numpy as np
+import utils.utils as utils
 
 from tensorflow.keras.layers import Input, Dense, Conv2D, BatchNormalization, Flatten, Lambda, Add, Subtract
+
 
 class ActorCriticNetwork:
     def __init__(self, state_size, action_size, learning_rate, name='DQNetwork'):
@@ -13,17 +16,17 @@ class ActorCriticNetwork:
         # build inputs
         i = Input(shape=state_size)
 
-        #build CNN
+        # build CNN
         # 84 x 84 x 4
-        x = Conv2D(32, (8, 8), strides=[4,4], activation='relu', padding='valid')(i)
+        x = Conv2D(32, (8, 8), strides=[4, 4], activation='relu', padding='valid')(i)
         x = BatchNormalization()(x)
 
         # 20 x 20 x 32
-        x = Conv2D(64, (4, 4), strides=[2,2], activation='relu', padding='valid')(x)
+        x = Conv2D(64, (4, 4), strides=[2, 2], activation='relu', padding='valid')(x)
         x = BatchNormalization()(x)
 
         # 9 x 9 x 64
-        x = Conv2D(128, (4, 4), strides=[2,2], activation='relu', padding='valid')(x)
+        x = Conv2D(128, (4, 4), strides=[2, 2], activation='relu', padding='valid')(x)
         x = BatchNormalization()(x)
 
         # 3 x 3 x 128
@@ -34,14 +37,78 @@ class ActorCriticNetwork:
         val = Dense(1)(val)
 
         pol_act = Dense(256, activation='relu')(x)
-        pol_act = Dense(action_size)(pol_act)
+        pol_act = Dense(action_size, activation='softmax')(pol_act)
 
         self.model = tf.keras.models.Model(i, (pol_act, val))
-        #self.model.compile(tf.keras.optimizers.Adam(learning_rate), loss=tf.keras.losses.Huber())
+        # self.model.compile(tf.keras.optimizers.Adam(learning_rate), loss=tf.keras.losses.Huber())
 
         self.model.summary()
         self.hot_action = np.identity(action_size, dtype=np.int)
 
     def get_action(self, state):
-        actions, val = self.model.predict(state.reshape((1, *state.shape)))
-        return self.hot_action[np.argmax(actions)]
+        actions_prob, critic_val = self.model.predict(state.reshape((1, *state.shape)))
+        actions_prob = np.squeeze(actions_prob)
+        idx = np.random.choice(range(self.action_size), p=actions_prob)
+        return self.hot_action[idx], actions_prob, critic_val
+
+    def train(self, env, episodes_num, max_steps, beta, gamma):
+        rewards = []
+        entropy_term = 0
+
+        all_lengths = []
+        average_lengths = []
+
+        stacked_frames = deque([np.zeros((84, 84), dtype=np.int) for _ in range(self.state_size[2])],
+                               maxlen=self.state_size[2])
+
+        for episode in range(episodes_num):
+            log_probs = []
+            values = []
+            rewards = []
+
+            env.new_episode()
+
+            state, stacked_frames = utils.stack_frames(stacked_frames, env.get_state().screen_buffer, True)
+
+            step = 0
+            done = False
+            while step < max_steps and not done:
+                # play episode
+                step += 1
+
+                state, stacked_frames = utils.stack_frames(stacked_frames, env.get_state().screen_buffer, False)
+
+                action, actions_prob, critic_val = self.get_action(state)
+
+                reward = env.make_action(action.tolist())
+                values.append(critic_val)
+                done = env.is_episode_finished()
+                rewards.append(reward)
+
+                log_prob = np.log(actions_prob * action)
+
+                # todo check if entropy is correct.
+                entropy = -np.sum(actions_prob * np.log(actions_prob))
+
+                log_probs.append(log_prob)
+                entropy_term += entropy
+
+                if done or step == max_steps:
+                    _, critic_value = self.model.predict(state.reshape((1, *state.shape)))
+
+                    critic_value = np.squeeze(critic_value)
+
+                    total_rewards = np.sum(rewards)
+
+                    if episode % 10 == 0:
+                        print(f"episode: {episode}, reward: {total_rewards}")
+
+            # learn
+            Q_vals = np.zeros(len(values))
+            Q_val = 0
+            for t in reversed(range(len(rewards))):
+                Q_val = rewards[t] + gamma * Q_val
+                Q_vals[t] = Q_val
+
+            # update actor critic
+            advantage = Q_vals - values
--- src/main.py
+++ src/main.py
@@ -30,66 +30,6 @@ state_size = (image_size[0], image_size[1], stack_size)
 learning_rate = float(config['NN']['learning_rate'])
 
 
-def train(env, nn_net : ActorCriticNetwork):
-    rewards = []
-    entropy_term = 0
-
-    all_lengths = []
-    average_lengths = []
-
-    episodes_num = int(config['NN']['episodes_num'])
-    max_steps = int(config['NN']['max_steps'])
-    epsilon = float(config['NN']['epsilon_greedy'])
-
-    stacked_frames = deque([np.zeros((84,84), dtype=np.int) for _ in range(stack_size)], maxlen=stack_size)
-
-    for episode in range(episodes_num):
-        log_probs = []
-        values = []
-        rewards = []
-
-        env.new_episode()
-        
-        state, stacked_frames = utils.stack_frames(stacked_frames, env.get_state().screen_buffer, True)
-
-        step = 0
-        done = False
-        while step < max_steps and not done:
-            # play episode
-            step += 1
-
-            state, stacked_frames = utils.stack_frames(stacked_frames, env.get_state().screen_buffer, False)
-            
-            if np.random.rand() < epsilon:
-                action = nn_net.hot_action[np.random.randint(0, len(nn_net.hot_action))]
-            else:
-                action = nn_net.get_action(state)
-
-            reward = env.make_action(action.tolist())
-            done = env.is_episode_finished()
-            rewards.append(reward)
-
-            if done or step == max_steps:
-                _, critic_value = nn_net.model.predict(state.reshape((1, *state.shape)))
-
-                critic_value = np.squeeze(critic_value)
-                
-                total_rewards = np.sum(rewards)
-                rewards.append(np.sum(total_rewards))
-
-                if episode % 10 == 0:
-                    print(f"episode: {episode}, reward: {total_rewards}\n")
-                
-        
-        # training
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--mode', type=str, required=True, help='"test" or "train" or "test_env"')
@@ -112,7 +52,14 @@ if __name__ == '__main__':
         
         actor_critic = ActorCriticNetwork(state_size, action_size, learning_rate)
 
-        train(env, actor_critic)
+
+        NN_config = config['NN']
+        episodes_num = int(NN_config['episodes_num'])
+        max_steps = int(NN_config['max_steps'])
+        beta = float(NN_config['beta'])
+        gamma = float(NN_config['gamma'])
+
+        actor_critic.train(env, episodes_num, max_steps, beta, gamma)
 
     elif args.mode == 'test':
         training = False
